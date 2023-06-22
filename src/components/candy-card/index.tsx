e7a813b78dd42808cdd92f6e15cbc5de59fac83a
yashyenugu <yashyenugu@gmail.com>
2023-06-22 14:06:04 +0530

    Fix image

--- src/components/candy-card/index.tsx
+++ src/components/candy-card/index.tsx
@@ -8,12 +8,14 @@ import {
   HStack,
   useNumberInput,
   Badge,
-  Flex,
+    Flex,
+  Box
 } from "@chakra-ui/react";
-import Image from "@/components/shared/image";
+// import Image from "@/components/shared/image";
 import { Candy } from "@/server/models/candy.model";
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
 import { addCandyToCart, removeCandyFromCart } from "@/store/modules/cart";
+import Image from "next/image";
 
 interface CandyCardProps {
   candy: Candy;
@@ -27,8 +29,12 @@ const CandyCard: React.FC<CandyCardProps> = ({ candy }) => {
   );
 
   return (
-    <Card>
-      <Image width={360} height={360} src={candy.photo.url} alt={candy.name} />
+    <Card height="full">
+      <Box position="relative" w="200px" h="200px" minH="200px" mx="auto">
+        <Image src={candy.photo.url} alt={candy.name} fill />
+      </Box>
+
+      {/* <Image width={360} height={360} src={candy.photo.url} alt={candy.name} /> */}
       <CardHeader pb={0} px={2}>
         <Text textColor="gray.400" fontSize={"sm"}>
           {(candy.vendor as any).name}
@@ -42,44 +48,49 @@ const CandyCard: React.FC<CandyCardProps> = ({ candy }) => {
         </Text>
       </CardHeader>
       <CardBody pt={0} px={2}>
-        <Text fontSize="sm" fontWeight="normal">
-          {candy.description}
-        </Text>
-        <Flex></Flex>
-        <Text mb={2} mt={4} fontWeight={"medium"}>
-          ₹ {candy.price}
-        </Text>
-
-        <HStack w="full" spacing={4} justifyContent="center">
-          {cartCandy ? (
-            <>
-              <Button
-                onClick={() => {
-                  dispatch(addCandyToCart({ candy }));
-                }}
-              >
-                +
-              </Button>
-              <Text>{cartCandy.itemsInCart}</Text>
-              <Button
-                onClick={() => {
-                  dispatch(removeCandyFromCart({ candy }));
-                }}
-              >
-                -
-              </Button>
-            </>
-          ) : (
-                          <Button
-                              w="full"
-              onClick={() => {
-                dispatch(addCandyToCart({ candy }));
-              }}
-            >
-              Add to Cart
-            </Button>
-          )}
-        </HStack>
+        <Flex direction="column" justifyContent="space-between" h="full">
+          <Box>
+            <Text fontSize="sm" fontWeight="normal">
+              {candy.description}
+            </Text>
+            <Flex></Flex>
+          </Box>
+          <Box>
+            <Text mb={2} mt={4} fontWeight={"medium"}>
+              ₹ {candy.price}
+            </Text>
+            <HStack w="full" spacing={4} justifyContent="center">
+              {cartCandy ? (
+                <>
+                  <Button
+                    onClick={() => {
+                      dispatch(addCandyToCart({ candy }));
+                    }}
+                  >
+                    +
+                  </Button>
+                  <Text>{cartCandy.itemsInCart}</Text>
+                  <Button
+                    onClick={() => {
+                      dispatch(removeCandyFromCart({ candy }));
+                    }}
+                  >
+                    -
+                  </Button>
+                </>
+              ) : (
+                <Button
+                  w="full"
+                  onClick={() => {
+                    dispatch(addCandyToCart({ candy }));
+                  }}
+                >
+                  Add to Cart
+                </Button>
+              )}
+            </HStack>
+          </Box>
+        </Flex>
       </CardBody>
     </Card>
   );
