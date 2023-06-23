5cbf5e46e8b3f4dcf07302591b91b74e7f105126
yashyenugu <yashyenugu@gmail.com>
2023-06-23 15:27:42 +0530

    Add Link Overlay to candy cards

--- src/components/candy-card/index.tsx
+++ src/components/candy-card/index.tsx
@@ -8,14 +8,17 @@ import {
   HStack,
   useNumberInput,
   Badge,
-    Flex,
-  Box
+  Flex,
+  Box,
+  LinkBox,
+  LinkOverlay,
 } from "@chakra-ui/react";
 // import Image from "@/components/shared/image";
 import { Candy } from "@/server/models/candy.model";
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
 import { addCandyToCart, removeCandyFromCart } from "@/store/modules/cart";
 import Image from "next/image";
+import NextLink from "next/link";
 
 interface CandyCardProps {
   candy: Candy;
@@ -29,70 +32,79 @@ const CandyCard: React.FC<CandyCardProps> = ({ candy }) => {
   );
 
   return (
-    <Card height="full">
-      <Box position="relative" w={{base: "150px",lg:"200px"}} h={{base: "150px",lg:"200px"}} minH={{base: "150px",lg:"200px"}} mx="auto">
-        <Image src={candy.photo.url} alt={candy.name} fill />
-      </Box>
+    <LinkBox>
+      <Card height="full">
+        <Box
+          position="relative"
+          w={{ base: "150px", lg: "200px" }}
+          h={{ base: "150px", lg: "200px" }}
+          minH={{ base: "150px", lg: "200px" }}
+          mx="auto"
+        >
+          <Image src={candy.photo.url} alt={candy.name} fill />
+        </Box>
 
-      {/* <Image width={360} height={360} src={candy.photo.url} alt={candy.name} /> */}
-      <CardHeader pb={0} px={2}>
-        <Text textColor="gray.400" fontSize={"sm"}>
-          {(candy.vendor as any).name}
-        </Text>
-
-        <Text mt={0} as="h3" fontWeight="medium" fontSize="xl">
-          {candy.name}
-          <Badge ml={2} colorScheme="purple">
-            New
-          </Badge>
-        </Text>
-      </CardHeader>
-      <CardBody pt={0} px={2}>
-        <Flex direction="column" justifyContent="space-between" h="full">
-          <Box>
-            <Text fontSize="sm" fontWeight="normal">
-              {candy.description}
+        {/* <Image width={360} height={360} src={candy.photo.url} alt={candy.name} /> */}
+        <CardHeader pb={0} px={2}>
+          <Text textColor="gray.400" fontSize={"sm"}>
+            {(candy.vendor as any).name}
+          </Text>
+          <LinkOverlay as={NextLink} href={`/candy/${candy._id}`}>
+            <Text mt={0} as="h3" fontWeight="medium" fontSize="xl">
+              {candy.name}
+              <Badge ml={2} colorScheme="purple">
+                New
+              </Badge>
             </Text>
-            <Flex></Flex>
-          </Box>
-          <Box>
-            <Text mb={2} mt={4} fontWeight={"medium"}>
-              ₹ {candy.price}
-            </Text>
-            <HStack w="full" spacing={4} justifyContent="center">
-              {cartCandy ? (
-                <>
+          </LinkOverlay>
+        </CardHeader>
+        <CardBody pt={0} px={2}>
+          <Flex direction="column" justifyContent="space-between" h="full">
+            <Box>
+              <Text fontSize="sm" fontWeight="normal">
+                {candy.description}
+              </Text>
+              <Flex></Flex>
+            </Box>
+            <Box>
+              <Text mb={2} mt={4} fontWeight={"medium"}>
+                ₹ {candy.price}
+              </Text>
+              <HStack w="full" spacing={4} justifyContent="center">
+                {cartCandy ? (
+                  <>
+                    <Button
+                      onClick={() => {
+                        dispatch(addCandyToCart({ candy }));
+                      }}
+                    >
+                      +
+                    </Button>
+                    <Text>{cartCandy.itemsInCart}</Text>
+                    <Button
+                      onClick={() => {
+                        dispatch(removeCandyFromCart({ candy }));
+                      }}
+                    >
+                      -
+                    </Button>
+                  </>
+                ) : (
                   <Button
+                    w="full"
                     onClick={() => {
                       dispatch(addCandyToCart({ candy }));
                     }}
                   >
-                    +
+                    Add to Cart
                   </Button>
-                  <Text>{cartCandy.itemsInCart}</Text>
-                  <Button
-                    onClick={() => {
-                      dispatch(removeCandyFromCart({ candy }));
-                    }}
-                  >
-                    -
-                  </Button>
-                </>
-              ) : (
-                <Button
-                  w="full"
-                  onClick={() => {
-                    dispatch(addCandyToCart({ candy }));
-                  }}
-                >
-                  Add to Cart
-                </Button>
-              )}
-            </HStack>
-          </Box>
-        </Flex>
-      </CardBody>
-    </Card>
+                )}
+              </HStack>
+            </Box>
+          </Flex>
+        </CardBody>
+      </Card>
+    </LinkBox>
   );
 };
 
