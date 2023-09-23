8171a87ac32dacbad5230dcd1c7dee3373cb5fd6
yashyenugu <yashyenugu@gmail.com>
2023-09-23 19:46:44 +0530

    Add super admin provider

--- src/components/provider/super-admin-provider/SuperAdminProvider.tsx
+++ src/components/provider/super-admin-provider/SuperAdminProvider.tsx
@@ -1,27 +1,26 @@
 import { useRouter } from "next/router";
 import React from "react";
 import { api } from "@/utils/api";
-import { Role } from "../../utils/types/user";
-import {Spinner} from '@chakra-ui/react'
+import { Role } from "@/utils/types/user";
+import { Spinner } from "@chakra-ui/react";
 
-const VendorProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
+const SuperAdminProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
   const router = useRouter();
   const { isLoading, data } = api.auth.user.useQuery();
 
   React.useEffect(() => {
     (async () => {
-      if (!isLoading && (!data || data.role !== Role.Vendor)) {
+      if (!isLoading && (!data || data.role !== Role.Admin)) {
         await router.replace("/");
       }
-    })()
-    
+    })();
   }, [router, data, isLoading]);
 
   if (isLoading) {
-    return <Spinner/>;
+    return <Spinner />;
   }
 
   return <>{children}</>;
 };
 
-export default VendorProvider;
+export default SuperAdminProvider;
