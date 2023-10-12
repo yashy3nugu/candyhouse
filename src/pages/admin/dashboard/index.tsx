0df1136bb0fd1cffd2b28bd1726386ba7ada03a8
yashyenugu <yashyenugu@gmail.com>
2023-10-12 20:14:22 +0530

    Add admin order page

--- src/pages/admin/dashboard/index.tsx
+++ src/pages/admin/dashboard/index.tsx
@@ -1,8 +1,11 @@
+import OrderDataTable from "@/components/order-data-table";
 import usePagination from "@/hooks/use-pagination/usePagination";
 import AdminLayout from "@/layouts/admin-layout/navbar";
 import { NextPageWithLayout } from "@/pages/_app";
 import { api } from "@/utils/api";
 import {
+  Container,
+  Heading,
   Table,
   TableCaption,
   TableContainer,
@@ -17,7 +20,7 @@ import {
 const AdminDashboard: NextPageWithLayout = () => {
   const { page, handleNextPage, handlePrevPage } = usePagination({});
 
-  const { data, isLoading } = api.order.all.useQuery({
+  const { data: orders, isLoading } = api.order.all.useQuery({
     limit: 10,
     page,
   });
@@ -27,29 +30,24 @@ const AdminDashboard: NextPageWithLayout = () => {
   }
 
   return (
-    <TableContainer>
-      <Table variant="simple">
-        <TableCaption>Current Orders</TableCaption>
-        <Thead>
-          <Tr>
-            <Th>Order ID</Th>
-            {/* <Th>Order Date</Th> */}
-            <Th isNumeric>Order Total (Rs)</Th>
-            <Th isNumeric>Order Status</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {data?.orders?.map((order) => (
-            <Tr key={order._id}>
-              <Td>{order._id}</Td>
-              {/* <Td>{order.createdAt.toDateString()}</Td> */}
-              <Td>{order.price}</Td>
-              <Td>{order.status}</Td>
-            </Tr>
-          ))}
-        </Tbody>
-      </Table>
-    </TableContainer>
+    <>
+      <Container
+        maxW="3xl"
+        py={{ base: "12", md: "24" }}
+        px={{ base: "0", sm: "8" }}
+      >
+        <Heading as="h1">Orders placed</Heading>
+        {orders && (
+          <OrderDataTable
+            {...{ page }}
+            data={orders}
+            linkUrl="/user/profile/orders"
+            nextPage={handleNextPage}
+            previousPage={handlePrevPage}
+          />
+        )}
+      </Container>
+    </>
   );
 };
 
