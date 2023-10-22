5d104107cda3a73fbe2e539df6689d1e157011ee
yashyenugu <yashyenugu@gmail.com>
2023-10-22 14:36:14 +0530

    Add login route

--- user-service/src/controllers/user.controller.ts
+++ user-service/src/controllers/user.controller.ts
@@ -2,10 +2,12 @@ import { UserModel } from "../models/user.model";
 import { ExpressResponse } from "../utils/types/express";
 import { Role } from "../utils/types/user";
 import * as jwt from "../lib/jwt";
+import { loginInputSchema, registerInputSchema } from "../utils/schemas/user";
+import { z } from "zod";
 
 export const signup: ExpressResponse = async (req, res, next) => {
   try {
-    const { name, email, password, role } = req.body;
+    const { name, email, password, role }: z.infer<typeof registerInputSchema> = req.body;
 
     // Check if a user with the same name already exists
     const existingUser = await UserModel.findOne({ name });
@@ -24,6 +26,33 @@ export const signup: ExpressResponse = async (req, res, next) => {
       password,
       role,
     });
+
+    const token = jwt.signToken({ user });
+
+    res.status(201).json({
+      token,
+      user,
+    });
+  } catch (err: any) {
+    next(err);
+  }
+};
+
+export const login: ExpressResponse = async (req, res, next) => {
+  try {
+    const {  email, password,  }: z.infer<typeof loginInputSchema> =
+      req.body;
+
+    // Check if a user with the same name already exists
+    const user = await UserModel.findOne({
+      email,
+    }).select("+password");
+
+    if (!user || !user.checkPassword(password, user.password)) {
+      return res.status(400).json({
+        message: "Invalid Credentials",
+      });
+    }
 
     const token = jwt.signToken({ user });
 
--- user-service/src/routers/user.router.ts
+++ user-service/src/routers/user.router.ts
@@ -1,26 +1,13 @@
 import express from "express";
 import * as userController from "../controllers/user.controller"
 import { validate } from "../middleware/validate.middleware";
-import { registerInputSchema } from "../utils/schemas/user";
+import { loginInputSchema, registerInputSchema } from "../utils/schemas/user";
 
 const router = express.Router();
 
-// router.use(authController.parseAuthCookie, authController.protectRoutes);
-
-// router.patch("/update-password", userController.updatePassword);
-
-// router.patch("/reject-friend-request", userController.rejectFriendRequest);
-
 router.post("/register", validate(registerInputSchema), userController.signup)
+router.post("/login", validate(loginInputSchema), userController.login);
 
-// router
-//   .route("/")
-//   .get(userController.getAllUsers)
-//   .patch(
-//     userController.uploadProfileImage,
-//     resizeImage("users"),
-//     userController.updateUserProfile
-//   );
 
 export default router;
 
