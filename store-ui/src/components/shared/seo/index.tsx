10480b76358f1593842f68720164226dddb48191
yashyenugu <yashyenugu@gmail.com>
2023-11-05 11:54:26 +0530

    Fix login and store

---
+++ store-ui/src/components/shared/seo/index.tsx
@@ -0,0 +1,24 @@
+import Head from "next/head";
+import React from "react";
+
+interface SeoProps {
+  title?: string;
+  children?: React.ReactNode;
+}
+
+const Seo: React.FC<SeoProps> = ({ title, children }) => {
+  const fullTitle = `Candy House ${title ? `| ${title}` : ""}`.trim();
+
+  return (
+    <>
+      <Head>
+        <title>{fullTitle}</title>
+        <meta property="og:title" content={fullTitle} />
+        <meta name="twitter:title" content={fullTitle} />
+        {children}
+      </Head>
+    </>
+  );
+};
+
+export default Seo;
